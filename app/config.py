c0105fd37471038e269e9aa51692eaa720b7f21f
rdfsx <m315498@gmail.com>
2021-09-16 11:45:57 +0300

    remove global var from config

--- app/config.py
+++ app/config.py
@@ -2,23 +2,23 @@ from typing import NamedTuple
 
 from environs import Env
 
-env = Env()
-env.read_env()
-
 
 class Config(NamedTuple):
-    BOT_TOKEN = env.str('BOT_TOKEN')
-    STATISTICS_TOKEN = env.str('STATISTICS_TOKEN')
+    __env = Env()
+    __env.read_env()
+
+    BOT_TOKEN = __env.str('BOT_TOKEN')
+    STATISTICS_TOKEN = __env.str('STATISTICS_TOKEN')
 
     ADMINS = [
-        env.str('ADMIN_ID')
+        __env.str('ADMIN_ID')
     ]
 
-    MONGODB_DATABASE = env.str('MONGODB_DATABASE')
-    MONGODB_USERNAME = env.str('MONGODB_USERNAME')
-    MONGODB_PASSWORD = env.str('MONGODB_PASSWORD')
-    MONGODB_HOSTNAME = env.str('MONGODB_HOSTNAME')
-    MONGODB_PORT = env.str('MONGODB_PORT')
+    MONGODB_DATABASE = __env.str('MONGODB_DATABASE')
+    MONGODB_USERNAME = __env.str('MONGODB_USERNAME')
+    MONGODB_PASSWORD = __env.str('MONGODB_PASSWORD')
+    MONGODB_HOSTNAME = __env.str('MONGODB_HOSTNAME')
+    MONGODB_PORT = __env.str('MONGODB_PORT')
     MONGODB_URI = 'mongodb://'
 
     if MONGODB_USERNAME and MONGODB_PASSWORD:
