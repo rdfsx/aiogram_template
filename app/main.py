c887785c2ccb3843062464515ca4230f15e0cbde
rdfsx <m315498@gmail.com>
2021-09-16 15:42:49 +0300

    added commands for admins only

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from app import handlers, middlewares, filters
 from app.config import Config
 from app.utils import logger
 from app.utils.notifications.startup_notify import notify_superusers
+from app.utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dp):
@@ -20,6 +21,8 @@ async def on_startup(dp):
     logger.setup_logger()
 
     await notify_superusers(Config.ADMINS)
+
+    await set_default_commands(dp)
 
 
 async def on_shutdown(dp):
--- app/utils/set_bot_commands.py
+++ app/utils/set_bot_commands.py
@@ -1,4 +1,7 @@
 from aiogram import types
+from aiogram.types import BotCommandScopeChat
+
+from app.config import Config
 
 
 async def set_default_commands(dp):
@@ -8,3 +11,12 @@ async def set_default_commands(dp):
             types.BotCommand("help", "Вывести справку"),
         ]
     )
+    for admin in Config.ADMINS:
+        await dp.bot.set_my_commands(
+            [
+                types.BotCommand("amount", "Количество пользователей в бд"),
+                types.BotCommand("exists_amount", "Количество живых пользователей"),
+                types.BotCommand("broadcast", "Рассылка по всем пользователям"),
+            ],
+            BotCommandScopeChat(admin)
+        )
