8b0e75edd9189de6d42190b4320f2a9644074167
rdfsx <m315498@gmail.com>
2021-09-28 16:14:51 +0300

    inirial commit

--- app/config.py
+++ app/config.py
@@ -1,25 +1,89 @@
-from typing import NamedTuple
+from dataclasses import dataclass
 
 from environs import Env
 
 
-class Config(NamedTuple):
-    __env = Env()
-    __env.read_env()
+@dataclass(frozen=True)
+class DbConfig:
+    host: str
+    password: str
+    username: str
+    database: str
+    uri: str
 
-    BOT_TOKEN = __env.str('BOT_TOKEN')
 
-    ADMINS = [
-        __env.str('ADMIN_ID')
-    ]
+@dataclass(frozen=True)
+class Bot:
+    token: str
+    admin_ids: list[int]
 
-    MONGODB_DATABASE = __env.str('MONGODB_DATABASE')
-    MONGODB_USERNAME = __env.str('MONGODB_USERNAME')
-    MONGODB_PASSWORD = __env.str('MONGODB_PASSWORD')
-    MONGODB_HOSTNAME = __env.str('MONGODB_HOSTNAME')
-    MONGODB_PORT = __env.str('MONGODB_PORT')
-    MONGODB_URI = 'mongodb://'
 
-    if MONGODB_USERNAME and MONGODB_PASSWORD:
-        MONGODB_URI += f"{MONGODB_USERNAME}:{MONGODB_PASSWORD}@"
-    MONGODB_URI += f"{MONGODB_HOSTNAME}:{MONGODB_PORT}"
+@dataclass(frozen=True)
+class Webhook:
+    host: str
+    port: int
+    path: str
+    url: str
+    public_cert: str
+    private_cert: str
+
+
+@dataclass(frozen=True)
+class WebApp:
+    host: str
+    port: int
+
+
+@dataclass(frozen=True)
+class Miscellaneous:
+    other_params: str = None
+
+
+@dataclass(frozen=True)
+class Config:
+    bot: Bot
+    db: DbConfig
+    webhook: Webhook
+    webapp: WebApp
+    misc: Miscellaneous
+
+
+def load_config(path: str = None):
+    env = Env()
+    env.read_env(path)
+
+    mongo_username = env.str("MONGODB_USERNAME")
+    mongo_password = env.str("MONGODB_PASSWORD")
+    mongo_hostname = env.str("MONGODB_HOSTNAME")
+    mongo_port = env.str("MONGODB_PORT")
+    mongo_uri = 'mongodb://'
+    if mongo_username and mongo_password:
+        mongo_uri += f"{mongo_username}:{mongo_password}@"
+    mongo_uri += f"{mongo_hostname}:{mongo_port}"
+
+    return Config(
+        bot=Bot(
+            token=env.str("BOT_TOKEN"),
+            admin_ids=list(map(int, env.list("ADMINS"))),
+        ),
+        db=DbConfig(
+            host=mongo_hostname,
+            password=mongo_password,
+            username=mongo_username,
+            database=env.str('MONGODB_DATABASE'),
+            uri=mongo_uri,
+        ),
+        webhook=Webhook(
+            host=env.str('WEBHOOK_HOST'),
+            port=env.int('WEBHOOK_PORT'),
+            path=env.str('WEBHOOK_PATH'),
+            url=env.str('WEBHOOK_URL'),
+            public_cert="webhook_cert.pem",
+            private_cert="webhook_pkey.pem",
+        ),
+        webapp=WebApp(
+            host=env.str('WEBHOOK_HOST'),
+            port=env.int('WEBHOOK_PORT'),
+        ),
+        misc=Miscellaneous()
+    )
--- app/main.py
+++ app/main.py
@@ -5,24 +5,28 @@ from aiogram.contrib.fsm_storage.mongo import MongoStorage
 from aiogram.utils.executor import start_polling
 
 from app import handlers, middlewares, filters
-from app.config import Config
+from app.config import Config, load_config
 from app.utils import logger
+from app.utils.certificates import get_ssh_certificate
 from app.utils.notifications.startup_notify import notify_superusers
 from app.utils.set_bot_commands import set_commands
 
 
 async def on_startup(dp):
+    config: Config = dp.bot.get('config')
+
     middlewares.setup(dp)
-
     filters.setup(dp)
-
     handlers.setup_all_handlers(dp)
-
     logger.setup_logger()
 
-    await notify_superusers(Config.ADMINS)
-
+    await notify_superusers(config.bot.admin_ids)
     await set_commands(dp)
+
+    await dp.bot.set_webhook(
+        config.webhook.url,
+        certificate=get_ssh_certificate(config.webhook.public_cert),
+    )
 
 
 async def on_shutdown(dp):
@@ -34,8 +38,10 @@ async def on_shutdown(dp):
 
 
 def main():
-    bot = Bot(token=Config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-    storage = MongoStorage(host=Config.MONGODB_HOSTNAME)
+    config = load_config(".env")
+    bot = Bot(token=config.bot.token, parse_mode=types.ParseMode.HTML)
+    storage = MongoStorage(host=config.db.host, port=config.db.port)
     dp = Dispatcher(bot, storage=storage)
+    bot['config'] = config
 
     start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
