0286da6df5314fc5229b6182dcb39c58e055612d
rdfsx <m315498@gmail.com>
2021-10-04 10:50:03 +0300

    fixed mongo

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.mongo import MongoStorage
 from aiogram.utils.executor import start_polling
+from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from app import handlers, middlewares, filters
 from app.config import Config
+from app.models.motor_client import MongoClient
 from app.utils import logger
 from app.utils.notifications.startup_notify import notify_superusers
 from app.utils.set_bot_commands import set_commands
@@ -13,15 +15,16 @@ from app.utils.set_bot_commands import set_commands
 
 async def on_startup(dp):
     middlewares.setup(dp)
-
     filters.setup(dp)
-
     handlers.setup_all_handlers(dp)
-
     logger.setup_logger()
 
-    await notify_superusers(Config.ADMINS)
+    client = MongoClient()
+    db = await client.get_db()
+    dp.bot["client"]: MongoClient = client
+    dp.bot["db"]: AsyncIOMotorDatabase = db
 
+    await notify_superusers(Config.ADMINS)
     await set_commands(dp)
 
 
@@ -30,6 +33,9 @@ async def on_shutdown(dp):
     await dp.bot.session.close()
     await dp.storage.close()
     await dp.storage.wait_closed()
+    if client := dp.bot.get('client', None):
+        await client.close()
+        await client.wait_closed()
     logging.warning("Bye!")
 
 
--- app/services/sync.py
+++ app/services/sync.py
@@ -4,11 +4,12 @@ from functools import wraps, partial
 from typing import Any, Awaitable
 
 
-def run_sync(func: Callable[..., Any]) -> Callable[[Any], Awaitable[Any]]:
+def run_sync(func: Callable[..., Any]):
 
     @wraps(func)
     async def wrapper(*args: Any, **kwargs: Any):
-        loop = asyncio.get_running_loop()
-        return await loop.run_in_executor(None, partial(func, *args, **kwargs))
+        return await asyncio.get_running_loop().run_in_executor(
+            None, partial(func, *args, **kwargs)
+        )
 
     return wrapper
