7eaa0c101884c1a501a3362211828dcdb46f66dd
rdfsx <m315498@gmail.com>
2021-10-17 14:30:05 +0300

    added updated_at field to models

--- app/handlers/private/default.py
+++ app/handlers/private/default.py
@@ -1,13 +1,19 @@
 from aiogram import Dispatcher
 from aiogram.types import Message
+from aiogram.utils.markdown import quote_html
 from odmantic import AIOEngine
 
 from app.models import UserModel
 
 
 async def get_default_message(m: Message, user: UserModel, db: AIOEngine):
-    await m.answer(m.text)
+    await m.answer(quote_html(user))
+
+
+async def edit_db(m: Message, user: UserModel, db: AIOEngine):
+    await db.save(UserModel(id=m.from_user.id, language='хер'))
 
 
 def setup(dp: Dispatcher):
+    dp.register_message_handler(edit_db, commands="edit")
     dp.register_message_handler(get_default_message)
--- app/models/chat.py
+++ app/models/chat.py
@@ -7,8 +7,10 @@ from odmantic import Field, Model
 class ChatModel(Model):
     id: int = Field(primary_field=True)
     type: str = Field(...)
-    created_at: datetime = Field(default_factory=datetime.now)
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: datetime = Field(default_factory=datetime.now)
 
     class Config:
         collection = "Chats"
         json_loads = json.loads
+        parse_doc_with_default_factories = True
