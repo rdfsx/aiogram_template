068a492079303c0285771b7a908d45f968ab9c95
rdfsx <m315498@gmail.com>
2021-10-17 14:30:34 +0300

    fixed default

--- app/handlers/private/default.py
+++ app/handlers/private/default.py
@@ -10,10 +10,5 @@ async def get_default_message(m: Message, user: UserModel, db: AIOEngine):
     await m.answer(quote_html(user))
 
 
-async def edit_db(m: Message, user: UserModel, db: AIOEngine):
-    await db.save(UserModel(id=m.from_user.id, language='хер'))
-
-
 def setup(dp: Dispatcher):
-    dp.register_message_handler(edit_db, commands="edit")
     dp.register_message_handler(get_default_message)
