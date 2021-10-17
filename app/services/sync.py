daea7c429ca5ad12c087d5d7e34920993ef5cbe3
rdfsx <m315498@gmail.com>
2021-10-17 13:49:50 +0300

    added run_sync function

--- app/services/sync.py
+++ app/services/sync.py
@@ -1,15 +1,21 @@
 import asyncio
 from collections.abc import Callable
 from functools import wraps, partial
-from typing import Any, Awaitable
+from typing import Any, TypeVar
+
+Result = TypeVar("Result")
 
 
-def run_sync(func: Callable[..., Any]):
+def run_sync_decorator(func: Callable[..., Any]):
 
     @wraps(func)
     async def wrapper(*args: Any, **kwargs: Any):
-        return await asyncio.get_running_loop().run_in_executor(
-            None, partial(func, *args, **kwargs)
-        )
+        loop = asyncio.get_event_loop()
+        return await loop.run_in_executor(None, partial(func, *args, **kwargs))
 
     return wrapper
+
+
+async def run_sync(func: Callable[..., Result], *args: Any, **kwargs: Any) -> Result:
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, partial(func, *args, **kwargs))
